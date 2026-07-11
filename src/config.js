3a6c47d22e2c9c80b3703baffd169e5da24e1ed9
Constantine-Li8U <2803268187@qq.com>
2026-07-12 00:00:26 +0800

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const QINIU_CONFIG = {
+    UPLOAD_URL: "https://upload-z2.qiniup.com",
+    AI_GATEWAY: "https://api-ai.qiniu.com/v1/engu-face",
+    BUCKET_DOMAIN: "http://your-bucket-domain.com"
+};
+
+export default QINIU_CONFIG;

ae69ef984891f167a0b50cb0b5028a20aa24c921
Constantine-Li8U <2803268187@qq.com>
2026-07-12 11:38:19 +0800

    Delete src directory

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const QINIU_CONFIG = {
-    UPLOAD_URL: "https://upload-z2.qiniup.com",
-    AI_GATEWAY: "https://api-ai.qiniu.com/v1/engu-face",
-    BUCKET_DOMAIN: "http://your-bucket-domain.com"
-};
-
-export default QINIU_CONFIG;
